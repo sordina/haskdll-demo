15cb626b48577d6cf53cfa26a3e40829c639c07b
Lyndon Maydwell <lyndon@sordina.net>
2021-03-25 21:40:56 +1100

    cleanup again

--- go.sh
+++ go.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 ghc --make -dynamic Main.hs
-ghc --make -dynamic -shared -fPIC Plugin1.hs -o plugins/plugin1-so
-ghc --make -dynamic -shared -fPIC Plugin2.hs -o plugins/plugin2-so
+ghc --make -dynamic -shared -fPIC Plugin1.hs -o plugin1-so
+ghc --make -dynamic -shared -fPIC Plugin2.hs -o plugin2-so
 
-PLUGIN=./plugins/plugin1-so ./Main
-PLUGIN=./plugins/plugin2-so ./Main
+rm -f Main.hi
+rm -f Main.o
+rm -f Plugin1.hi
+rm -f Plugin1.o
+rm -f Plugin2.hi
+rm -f Plugin2.o
+
+PLUGIN=./plugin1-so ./Main
+PLUGIN=./plugin2-so ./Main
